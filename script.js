1682a23add2acd0addf50914cf09130e7aaa0d4f
lq0502 <148927487+lq0502@users.noreply.github.com>
2023-10-25 21:18:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ document.addEventListener('DOMContentLoaded', function () {
         audioElements[note] = audio;
     }
 
-    // 预加载所有音频
     const notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'Cc4', 'Dd4', 'Ff4', 'Gg4', 'Aa4'];
     notes.forEach(note => preloadAudio(note));
 
@@ -48,64 +47,51 @@ document.addEventListener('DOMContentLoaded', function () {
         switch (key) {
             case 'KeyQ':
             case 'Keyq':
-                // 播放 C4 音符
                 playSound(document.getElementById('C4'));
                 break;
             case 'Digit2':
-                // 播放 C4 音符
                 playSound(document.getElementById('Cc4'));
                 break;
             case 'KeyW':
             case 'Keyw':
-                // 播放 C4 音符
                 playSound(document.getElementById('D4'));
                 break;
             case 'Digit3':
-                // 播放 C4 音符
                 playSound(document.getElementById('Dd4'));
                 break;
             case 'KeyE':
             case 'Keye':
-                // 播放 C4 音符
                 playSound(document.getElementById('E4'));
                 break;
             case 'KeyR':
             case 'Keyr':
-                // 播放 C4 音符
                 playSound(document.getElementById('F4'));
                 break;
             case 'Digit5':
-                // 播放 C4 音符
                 playSound(document.getElementById('Ff4'));
                 break;
             case 'KeyT':
             case 'Keyt':
-                // 播放 C4 音符
                 playSound(document.getElementById('G4'));
                 break;
             case 'Digit6':
-                // 播放 C4 音符
                 playSound(document.getElementById('Gg4'));
                 break;
             case 'KeyY':
             case 'Keyy':
-                // 播放 C4 音符
                 playSound(document.getElementById('A4'));
                 break;
             case 'Digit7':
-                // 播放 C4 音符
                 playSound(document.getElementById('Aa4'));
                 break;
             case 'KeyU':
             case 'Keyu':
-                // 播放 C4 音符
                 playSound(document.getElementById('B4'));
                 break;
             case 'KeyI':
             case 'Keyi':
-                // 播放 C4 音符
                 playSound(document.getElementById('C5'));
-                break;// 处理其他键位...
+                break;
         }
     });
 });
